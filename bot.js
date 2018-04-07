761af5f3d277611b8125fbcc11f06318047ebc26
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-07 19:33:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const db = require('quick.db');
+const prefix = '!';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
+    client.user.setActivity('over RoGiveaway | !help', { type: 'WATCHING' });
+    client.user.setStatus("online");
+});
 
 client.on('ready', () => {
     console.log('I am ready!');
