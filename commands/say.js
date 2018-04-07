512a3aefaa11eae0b02446393590dd4837d60558
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-07 19:38:49 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,18 @@
+exports.run = (Discord, client, message, args) => {
 
+let sender = message.author;
+let allowedRole = message.guild.roles.find("name", "botadmin");
+
+       if(message.member.roles.has(allowedRole.id)) {
+        const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+        
+       } else {
+         message.channel.send("This command is limited to bot admins only.")
+         message.delete()
+         .then(m=>m.delete(10000))
+       }
+}
