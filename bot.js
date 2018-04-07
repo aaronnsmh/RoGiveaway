f5623c64cd9333131d16b9c3d5f0df3dcb30f075
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-07 19:35:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,5 +19,39 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {    
+    
+    let sender = message.author;
+
+    if (sender.bot) return;
+    if (message.channel.type === 'dm') {
+        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: https://discord.gg/MKHXv2E) to use the bot! **")
+        return;
+    }
+
+        let msg = message.content.toLowerCase();
+        let args = message.content.slice(prefix.length).trim().split(" ");
+        let cmd = args.shift().toLowerCase();
+
+        if (!message.content.startsWith(prefix)) return;
+
+        try {
+
+            let commandFile = require(`./commands/${cmd}.js`);
+            commandFile.run(Discord, client, message, args);
+
+        } catch (e) {
+
+            console.log(e);
+
+        } finally {
+
+            console.log(`${message.author.username} ran the command: ${cmd} sucessfully!`);
+
+        }
+    })
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
