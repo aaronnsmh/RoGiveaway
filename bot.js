ee7f1f3ed452346ceb7eb4c0e4d0ec2ec7a2615e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-08 10:38:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
 const db = require('quick.db');
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
@@ -51,7 +53,14 @@ client.on('message', message => {
     })
 
 client.on('guildMemberAdd', member => {
-       member.send("Welcome to the server!");
+    var embedinfo = new Discord.RichEmbed()
+        .setFooter("© RoGiveaway Bot | Made by Aaron #1742")
+        .setColor(randomColor)
+        .setTitle("📖 RoGiveaway Information")
+        .setDescription("Welcome to the RoGiveaway server! Reflictance and Tinsel_Tay are pleased to have you join our server! Be prepared to be amazed with many more giveaways.")
+        .addField("What is RoGiveaway?","RoGiveaway is a leader in robux and account giveaways on Roblox. With hundreds of members already enjoying the service we provide!")
+        .addField("How do I win?","It's easy! Just check the giveaways channel for giveaways, when one is hosted, all you need to do is react to the message with a :tada:. Once a giveaway has finished, if you won the giveaway bot will ping you, simply DM the host to claim your prize! It's that easy")
+     member.sendEmbed(embedinfo);
        console.log(`${member.user.username} has joined`);
 });
 
