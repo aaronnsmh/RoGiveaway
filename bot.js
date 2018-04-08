2cb82b91ec44048b79297f93bdce9731741bc41d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-08 10:06:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
+const db = require('quick.db');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
