fd46988638513b36d9e3189869c638de80fa5c9d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-08 10:57:20 +0100

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,14 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+exports.run = (Discord, client, message, args) => {
+
+var embedinfo = new Discord.RichEmbed()
+        .setFooter("© RoGiveaway Bot | Made by Aaron #1742")
+        .setColor(randomColor)
+        .setTitle("📖 RoGiveaway Information")
+        .setDescription("Welcome to the RoGiveaway server! Reflictance and Tinsel_Tay are pleased to have you join our server! Be prepared to be amazed with many more giveaways.")
+        .addField("What is RoGiveaway?","RoGiveaway is a leader in robux and account giveaways on Roblox. With hundreds of members already enjoying the service we provide!")
+        .addField("How do I win?","It's easy! Just check the giveaways channel for giveaways, when one is hosted, all you need to do is react to the message with a :tada:. Once a giveaway has finished, if you won the giveaway bot will ping you, simply DM the host to claim your prize! It's that easy")
+member.sendEmbed(embedinfo);
+
+}
