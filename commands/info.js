fa4e9622f62ea5793d8b44a08ba8bdc1be62bb4e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-08 11:00:24 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -9,6 +9,6 @@ var embedinfo = new Discord.RichEmbed()
         .setDescription("Welcome to the RoGiveaway server! Reflictance and Tinsel_Tay are pleased to have you join our server! Be prepared to be amazed with many more giveaways.")
         .addField("What is RoGiveaway?","RoGiveaway is a leader in robux and account giveaways on Roblox. With hundreds of members already enjoying the service we provide!")
         .addField("How do I win?","It's easy! Just check the giveaways channel for giveaways, when one is hosted, all you need to do is react to the message with a :tada:. Once a giveaway has finished, if you won the giveaway bot will ping you, simply DM the host to claim your prize! It's that easy")
-member.sendEmbed(embedinfo);
+member.author.sendEmbed(embedinfo);
 
 }
